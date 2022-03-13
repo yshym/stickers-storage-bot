8619df67e1d7ab3a1402109636ed60e2648a93eb
Yevhen Shymotiuk <yshym@pm.me>
2022-03-13 16:04:26 +0200

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Stickers storage bot";
+
+  inputs = {
+    nixpkgs.url = "github:yevhenshymotiuk/nixpkgs/release-21.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = with pkgs;
+          mkShell {
+            nativeBuildInputs = [ gnumake ];
+          };
+      });
+}
